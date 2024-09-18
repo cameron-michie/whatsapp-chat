3a73b33c2fc1dfa409692e6d9e4581db9debd584
Cam Michie <cameron.oscar.michie@gmail.com>
2024-09-18 16:11:18 +0100

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+import { Inter } from "next/font/google";
+import { ClerkLoaded, ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Clover"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+
+          <nav className="flex justify-between p-5 border-b border-gray-200">
+            <h1 className="font-bold">Clover</h1>
+            <SignedOut>
+              <SignInButton mode='modal' />
+            </SignedOut>
+            <SignedIn>
+              <UserButton showName afterSignOutUrl="/" />
+            </SignedIn>
+          </nav>
+
+          <ClerkLoaded>
+            {children}
+          </ClerkLoaded>
+
+        </body>
+      </html>
+    </ClerkProvider >
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return <div className="p-5 max-w-md" >
+    <h1 className="text-7xl font-bold">
+      Authenticated Next Chat 🚀
+    </h1>
+    <p>Welcome to this chat app built with Next, Ably, and Clerk.</p>
+  </div>
+}
